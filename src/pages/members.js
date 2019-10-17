5ebe1deb5022ae4a62df5de70e4aa5e7e0edc271
Camilo Mendoza <camendoza94@gmail.com>
2019-10-17 16:02:32 -0500

    Members

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,23 +1,93 @@
 import React, { Fragment } from "react"
 import { graphql } from "gatsby"
 import Header from "../components/header"
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+import CardDeck from "react-bootstrap/CardDeck"
+import "./members.css"
 
 export default ({ data }) => {
   return (
     <Fragment>
       <Header/>
-      <h1>Professors</h1>
-      {data.allMembersJson.edges.map(n => {
-        if (n.node.category === "professor")
-          return <div key={n.node.name}>{n.node.name}</div>
-        return "";
-      })}
-      <h1>PHD Candidates</h1>
-      {data.allMembersJson.edges.map(n => {
-        if (n.node.category === "phd")
-          return <div key={n.node.name}>{n.node.name}</div>
-        return "";
-      })}
+      <Container className="mt-3">
+        {data.allMembersJson.edges.find(n => n.node.category === "professor") ?
+          <Fragment>
+            <h1>Professors</h1>
+            <CardDeck>
+              {data.allMembersJson.edges.map(n => {
+                if (n.node.category === "professor")
+                  return <Card className="border-0 shadow mb-4">
+                    <Card.Img variant="top" src="https://via.placeholder.com/350x150"/>
+                    <Card.Body>
+                      <Card.Text className="text-center">
+                        <h5>{n.node.name}</h5>
+                        {n.node.subCategory}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                return ""
+              })}
+            </CardDeck>
+          </Fragment> : ""}
+        {data.allMembersJson.edges.find(n => n.node.category === "phd" && n.node.state === "active") ?
+          <Fragment>
+            <h1>PHD Candidates</h1>
+            <CardDeck>
+              {data.allMembersJson.edges.map(n => {
+                if (n.node.category === "phd" && n.node.state === "active")
+                  return <Card className="border-0 shadow mb-4">
+                    <Card.Img variant="top" src="https://via.placeholder.com/350x150"/>
+                    <Card.Body>
+                      <Card.Text className="text-center">
+                        <h5>{n.node.name}</h5>
+                        {n.node.subCategory}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                return ""
+              })}
+            </CardDeck>
+          </Fragment> : ""}
+        {data.allMembersJson.edges.find(n => n.node.category === "msc" && n.node.state === "active") ?
+          <Fragment>
+            <h1>Master's Degree Candidates</h1>
+            <CardDeck>
+              {data.allMembersJson.edges.map(n => {
+                if (n.node.category === "msc" && n.node.state === "active")
+                  return <Card className="border-0 shadow mb-4">
+                    <Card.Img variant="top" src="https://via.placeholder.com/350x150"/>
+                    <Card.Body>
+                      <Card.Text className="text-center">
+                        <h5>{n.node.name}</h5>
+                        {n.node.subCategory}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                return ""
+              })}
+            </CardDeck>
+          </Fragment> : ""}
+        {data.allMembersJson.edges.find(n => n.node.category === "bsc" && n.node.state === "active") ?
+          <Fragment>
+            <h1>Bachelor's Degree Candidates</h1>
+            <CardDeck>
+              {data.allMembersJson.edges.map(n => {
+                if (n.node.category === "bsc" && n.node.state === "active")
+                  return <Card className="border-0 shadow mb-4">
+                    <Card.Img variant="top" src="https://via.placeholder.com/350x150"/>
+                    <Card.Body>
+                      <Card.Text className="text-center">
+                        <h5>{n.node.name}</h5>
+                        {n.node.subCategory}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                return ""
+              })}
+            </CardDeck>
+          </Fragment> : ""}
+      </Container>
     </Fragment>
   )
 }
@@ -31,6 +101,7 @@ export const query = graphql`
         subCategory
         category
         url
+        state
       }
     }
   }
